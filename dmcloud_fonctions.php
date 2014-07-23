925be669ff3893c8c80fec13abf30b17ac809594
magikcypress <cyp@rouquin.me>
2014-07-23 17:48:25 +0200

    ajout date

--- dmcloud_fonctions.php
+++ dmcloud_fonctions.php
@@ -12,22 +12,21 @@ function dmcloud_recup_video($page=1) {
     $cloudkey = new CloudKey($user_id, $api_key);
 
     # List some video and display the media_id and title:
-    $res = $cloudkey->media->list(array('fields' => array('id', 'meta.title', 'meta.author', 'meta.url'), 'per_page' => 20, 'page' => 1));
+    $res = $cloudkey->media->list(array('fields' => array('id', 'meta.title'), 'per_page' => 20, 'page' => 1));
     foreach($res->list as $media) {
         $url_vid = $cloudkey->media->get_stream_url(array('id' => $media->id));
-        // $info = $cloudkey->media->info(array('id' => $media->id, 'fields' => array('id', 'assets.flv_h263_mp3.download_url')));
-        // spip_log($info, 'test.' . _LOG_ERREUR);
-        //$author_vid = $cloudkey->media->set_meta(array('id' => $media->id,  'keys' => array('title' => $media->keys->title)));
-        // $assets = $author_vid = $cloudkey->media->get_assets(array('id' =>$media->id));
-        spip_log($author_vid, 'test.' . _LOG_ERREUR);
-        // spip_log($assets, 'test.' . _LOG_ERREUR);
+        $info = $cloudkey->media->info(array('id' => $media->id, 'fields' => array('created')));
+        $creer = $info->created;
+        // $info2 = $cloudkey->media->get_assets(array('id' => $media->id));
+        // spip_log($info2, 'test.' . _LOG_ERREUR);
+
         $list_dmcloud[] = array(
                 'id' => $media->id, 
-                'title' => $media->meta->title,
-                'author' => 'null',
+                'titre' => $media->meta->title,
+                'auteur' => $auteur,
+                'creer' => strftime("%c", $creer),
                 'url' => $url_vid);
     }
-    // spip_log($list_dmcloud, 'test.' . _LOG_ERREUR);
     return $list_dmcloud;
 
 }
--- genie/dmcloud.php
+++ genie/dmcloud.php
@@ -10,23 +10,5 @@ function genie_dmcloud_dist($t) {
     $api_key = lire_config('dmcloud/api_key_dmcloud');
     include_once(_DIR_PLUGIN_DMCLOUD."lib/cloudkey-php/CloudKey.php");
 
-    $cloudkey = new CloudKey($user_id, $api_key);
-
-    $result = $cloudkey->media->list(array('fields' => array('id'), 'per_page' => 20, 'page' => 2));
-
-
-    # List some video and display the media_id and title:
-    $res = $cloudkey->media->list(array('fields' => array('id', 'meta.title', 'meta.author', 'meta.url'), 'per_page' => 20, 'page' => 1));
-    foreach($res->list as $media) {
-        $url_vid = $cloudkey->media->get_stream_url(array('id' =>$media->id));
-        //$author_vid = $cloudkey->user->get(array('id' =>$media->id));
-        $list_dmcloud[] = array(
-                'id' => $media->id, 
-                'title' => $media->meta->title,
-                'author' => $author_vid->username,
-                'url' => $url_vid);
-    }
-    return $list_dmcloud;
-
     return 0;
 }
